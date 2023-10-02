098dfd4d35040d6d87f12281f2e57ed9322b2039
RIBINZX <muhdribin@gmail.com>
2023-10-02 13:18:50 +0530

    first commit

--- web/urls.py
+++ web/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 app_name = "web"
 
 urlpatterns = [
-
+    path("", views.IndexView.as_view(), name="index"),
 ]
--- web/views.py
+++ web/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'web/index.html'
+
